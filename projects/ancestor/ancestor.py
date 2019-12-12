b4a47ff897a62b35d53ef417dd2bc6229ce881a7
Rory Murray <rory.christian.murray@gmail.com>
2019-12-11 23:13:30 -0500

    Social

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -80,4 +80,4 @@ def earliest_ancestor(ancestors, starting_node):
 
 test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
 
-print(earliest_ancestor(test_ancestors, 2))
+print(earliest_ancestor(test_ancestors, 11))
--- projects/social/social.py
+++ projects/social/social.py
@@ -1,3 +1,4 @@
+from util import Queue
 import random
 
 class User:
@@ -47,7 +48,6 @@ class SocialGraph:
         self.last_id = 0
         self.users = {}
         self.friendships = {}
-        # !!!! IMPLEMENT ME
 
         # avg_friendships = total_friendships/num_users
 
@@ -69,22 +69,39 @@ class SocialGraph:
             for friendID in range(userID + 1, self.last_id + 1):
                 possibleFriendships.append((userID, friendID))
         
-        print(f"\npossibleFriendships : {possibleFriendships}\n")
+        # print(f"\npossibleFriendships : {possibleFriendships}\n")
 
         # shuffle list
         random.shuffle(possibleFriendships)
-        print(f"\nrandom friendships : {possibleFriendships}\n")
+        # print(f"\nrandom friendships : {possibleFriendships}\n")
 
 
         # slice off total_friendships from front to create friendships
         totalFriendships = avgFriendships * numUsers // 2
-        print(f"\nfriendships to create : {totalFriendships}\n")
+        # print(f"\nfriendships to create : {totalFriendships}\n")
         for i in range(totalFriendships):
             friendship = possibleFriendships[i]
             self.add_friendship(friendship[0], friendship[1])
 
 
-      
+    def bft_social(friendship_graph, starting_node, visited):
+        q = Queue()
+        visited = {}
+        path1 = [starting_node]
+
+        while q.size() > 0:
+            path = q.dequeue()
+            vertex = path[-1]
+            if vertex not in visited:
+                visited.add(vertex)
+            
+            for next_vertex in friendship_graph[starting_node]:
+                new_path = list(path)
+                new_path.append(next_vertex)
+                all_paths.append(new_path)
+
+
+        return None
 
 
     def get_all_social_paths(self, user_id):
@@ -96,18 +113,44 @@ class SocialGraph:
 
         The key is the friend's ID and the value is the path.
         """
-        visited = {}  # Note that this is a dictionary, not a set
-        # !!!! IMPLEMENT ME
+        ## perform a bft on user_id
+
+        # create empty queue
+        q = Queue()
+        visited = {}
+        # add a path to the starting node to the queue
+        q.enqueue([user_id])
+
+        # while q is not empty
+        while q.size() > 0:
+            # dequeue the the first 'path' out of the queue
+            p = q.dequeue()
+            # get the vertex (last item in path)
+            v = p[-1]
+            # check if visited
+            if v not in visited:
+                # if not mark as visited
+                visited[v] = p
+                # add a path to each friend to back of queue
+                for friend_id in self.friendships[v]:
+                    path_copy = p.copy()
+                    path_copy.append(friend_id)
+                    q.enqueue(path_copy)
+
         return visited
 
 
 if __name__ == '__main__':
     sg = SocialGraph()
-    sg.populate_graph(11, 3)
-    print("\nUSERS: ")
-    print(sg.users)
+    sg.populate_graph(1000, 5)
+    # print("\nUSERS: ")
+    # print(sg.users)
     print("\nFRIENDSHIPS: ")
     print(sg.friendships)
     print("\n")
+    # connections = sg.get_all_social_paths(1)
+    # print(connections)
+    print(f"\nsg.get_all_social_paths()\n")
     connections = sg.get_all_social_paths(1)
-    print(connections)
+    print(f"\n{connections}\n")
+    print("\n")
